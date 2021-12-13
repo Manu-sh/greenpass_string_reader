5f543c434f7318774d8b0e90d00e78a698510f75
Manu-sh <s3gmentationfault@gmail.com>
2021-12-13 16:15:40 +0100

    buffer zlib decompression - fix logic error

--- include/my_zlib.h
+++ include/my_zlib.h
@@ -2,8 +2,29 @@
 #include <zlib.h>
 #include <assert.h>
 
-#ifndef CHUNK
-    #define CHUNK 16384
+
+#if 0
+/*
+     * This is an ugly hack required to avoid corruption of the input and output data on Windows/MS-DOS systems.
+     * Without this,
+     * those systems would assume that the input and output files are text, and try to convert
+     * the end-of-line characters from one standard to another.
+     * That would corrupt binary data, and in particular would render the compressed data unusable.
+     * This sets the input and output to binary which suppresses the end-of-line conversions.
+     * SET_BINARY_MODE() will be used later on stdin and stdout, at the beginning of main().
+     */
+
+    #if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+    #  include <fcntl.h>
+    #  include <io.h>
+    #  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+    #else
+    #  define SET_BINARY_MODE(file)
+    #endif
+
+    /* avoid end-of-line conversions */
+    SET_BINARY_MODE(file_in);
+    SET_BINARY_MODE(file_out);
 #endif
 
 // slightly edited version of zerr() https://zlib.net/zlib_how.html
@@ -13,9 +34,9 @@ static inline void z_err(FILE *istream, FILE *ostream, int ret) {
     switch (ret) {
         case Z_ERRNO:
             if (ferror(istream))
-                fputs("error reading stdin\n", stderr);
+                fputs("error reading istream\n", stderr);
             if (ferror(ostream))
-                fputs("error writing stdout\n", stderr);
+                fputs("error writing ostream\n", stderr);
             break;
         case Z_STREAM_ERROR:
             fputs("invalid compression level\n", stderr);
@@ -32,15 +53,16 @@ static inline void z_err(FILE *istream, FILE *ostream, int ret) {
 
 }
 
-
 // slightly edited? version of inf() https://zlib.net/zlib_how.html
 static int zlib_decompress(FILE *source, FILE *dest) {
+
+    static const int Z_CHUNK = 16384;
 
     int ret;
     unsigned have;
     z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
+    unsigned char in[Z_CHUNK];
+    unsigned char out[Z_CHUNK];
 
     /* allocate inflate state */
     strm.zalloc = Z_NULL;
@@ -55,7 +77,7 @@ static int zlib_decompress(FILE *source, FILE *dest) {
     /* decompress until deflate stream ends or end of file */
     do {
 
-        strm.avail_in = fread(in, 1, CHUNK, source);
+        strm.avail_in = fread(in, 1, Z_CHUNK, source);
         if (ferror(source)) {
             (void)inflateEnd(&strm);
             return Z_ERRNO;
@@ -68,7 +90,7 @@ static int zlib_decompress(FILE *source, FILE *dest) {
         /* run inflate() on input until output buffer not full */
         do {
 
-            strm.avail_out = CHUNK;
+            strm.avail_out = Z_CHUNK;
             strm.next_out = out;
 
             ret = inflate(&strm, Z_NO_FLUSH);
@@ -82,7 +104,7 @@ static int zlib_decompress(FILE *source, FILE *dest) {
                     return ret;
             }
 
-            have = CHUNK - strm.avail_out;
+            have = Z_CHUNK - strm.avail_out;
             if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
                 (void)inflateEnd(&strm);
                 return Z_ERRNO;
@@ -90,10 +112,31 @@ static int zlib_decompress(FILE *source, FILE *dest) {
 
         } while (strm.avail_out == 0);
 
-    /* done when inflate() says it's done */
+        /* done when inflate() says it's done */
     } while (ret != Z_STREAM_END);
 
     /* clean up and return */
     (void)inflateEnd(&strm);
     return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+}
+
+// the buffer returned by this function should be free()
+static unsigned char * zlib_mem_decompress(unsigned char *z_src, size_t z_bsize, size_t *len) {
+
+    unsigned char *dst = NULL;
+
+    // zlib decompress
+    FILE * file_in = fmemopen(z_src, z_bsize, "r");
+    FILE * file_out = open_memstream((char **)&dst, len);
+
+    int err = Z_OK;
+    if ((err = zlib_decompress(file_in, file_out)) != Z_OK) {
+        z_err(file_in, file_out, err);
+        fclose(file_in), fclose(file_out), free(dst);
+        *len = 0;
+        return NULL;
+    }
+
+    fclose(file_in), fclose(file_out);
+    return dst;
 }
--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #include <base45.h>
 #include <base45.c> /* yes.. i don't care */
 
-#include <my_common.h>
 #include <my_zlib.h>
 #include <my_cbor.h>
 
@@ -53,11 +52,8 @@ int main(int argc, char *argv[]) {
     // https://github.com/jojo2234/GreenPass-Experiments
     const char *in = "NCFOXNEG2NBJ5*H:QO-.OMBN+XQ99N*6RFS5*TCVWBM*4ODMS0NSRHAL9.4I92P*AVAN9I6T5XH4PIQJAZGA2:UG%U:PI/E2$4JY/KB1TFTJ:0EPLNJ58G/1W-26ALD-I2$VFVVE.80Z0 /KY.SKZC*0K5AFP7T/MV*MNY$N.R6 7P45AHJSP$I/XK$M8TH1PZB*L8/G9HEDCHJ4OIMEDTJCJKDLEDL9CVTAUPIAK29VCN 1UTKFYJZJAPEDI.C$JC7KDF9CFVAPUB1VCSWC%PDMOLHTC$JC3EC66CTS89B9F$8H.OOLI7R3Y+95AF3J6FB5R8QMA70Z37244FKG6T$FJ7CQRB0R%5 47:W0UFJU.UOJ98J93DI+C0UEE-JEJ36VLIWQHH$QIZB%+N+Y2AW2OP6OH6XO9IE5IVU$P26J6 L6/E2US2CZU:80I7JM7JHOJKYJPGK:H3J1D1I3-*TW CXBD+$3PY2C725SS+TDM$SF*SHVT:5D79U+GC5QS+3TAQS:FLU+34IU*9VY-Q9P9SEW-AB+2Q2I56L916CO8T C609O1%NXDU-:R4TICQA.0F2HFLXLLWI8ZU53BMQ2N U:VQQ7RWY91SV2A7N3WQ9J9OAZ00RKLB2";
 
-    unsigned char zbuf[CHUNK] = {0}; // compressed
-    unsigned char buf[CHUNK] = {0};
-
+    unsigned char zbuf[16384] = {0}; // compressed
     size_t zbsize = sizeof zbuf;
-    const size_t bsize  = sizeof buf;
 
     // b45 decode
     if (base45_decode(zbuf, &zbsize, in, 0)) {
@@ -65,24 +61,17 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    zbsize = sizeof zbuf; // just for sure
-
     // zlib decompress
-    FILE * file_in = fmemopen(zbuf, zbsize, "r");
-    FILE * file_out = fmemopen(buf, bsize, "w");
-
-    int err = Z_OK;
-    if ((err = zlib_decompress(file_in, file_out)) != Z_OK) {
-        z_err(file_in, file_out, err);
+    unsigned char *buf;
+    size_t bsize = 0;
+    if (!(buf = zlib_mem_decompress(zbuf, zbsize, &bsize)))
         return EXIT_FAILURE;
-    }
 
-    fclose(file_in);
-    fclose(file_out);
 
     // CBOR/COSE data decode
-    // assert(strlen(buf) == labs((unsigned char *)memchr(buf, 0, bsize) - buf));
-    cbor_unserialize(buf, labs((unsigned char *)memchr(buf, 0, bsize) - buf));
+    cbor_unserialize(buf, bsize);
+
+    free(buf);
     return EXIT_SUCCESS;
 }
 
